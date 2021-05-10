2bb3939dde83cafa1e0bbf3c014c3cea19270a38
javascripttest <83923046+javascripttest@users.noreply.github.com>
2021-05-10 13:10:07 +0600

    Delete project — копия directory

--- project — копия/js/login.js
+++
@@ -1,6 +0,0 @@
-const name = prompt('Есіміңізді енгізіңіз:');
-if (name) {
-  document.querySelector('#welcome').innerHTML = `<b>${name}</b>, тестілеу бетіне қош келдіңіз!`;
-} else {
-  document.querySelector('#welcome').innerHTML = '<b>Құрметті тесттен өтуші</b>, тестілеу бетіне қош келдіңіз!';
-}
